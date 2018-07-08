498a76b1f8c1262da7baeff13c80c87c705db087
Mohsin <mohammadmohsin@gmail.com>
2018-07-08 16:38:40 +0500

    All tests passing

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -65,14 +65,47 @@ describe('Campaigns', () => {
     }
   })
 
-  it('allows a manger to make a payment request', async () => {
+  it('allows a manager to make a payment request', async () => {
     await campaign.methods
-                .createRequest('Buy batteries', '100', accounts[1])
-                .send({
-                  from: accounts[0],
-                  value: '1000000'
-                })
-    const request = await campaign.methods.requests(0).call()
-    assert.equal('Buy batteries', request.description)
+      .createRequest('Buy batteries', '100', 1, accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+    const request = await campaign.methods.requests(0).call();
+
+    assert.equal('Buy batteries', request.description);
+  });
+
+
+  it('processes request', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    })
+
+    await campaign.methods
+      .createRequest('Description doesnt really matter', web3.utils.toWei('5'), 1, accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      })
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    })
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    })
+
+    let balance = await web3.eth.getBalance(accounts[1])
+    balance = web3.utils.fromWei(balance, 'ether')
+    balance = parseFloat(balance)
+
+    console.log(balance)
+    assert(balance > 104)
   })
 })
