9767f94738ced152bd2db99dbc2ce5b6b1d08b80
Mohsin <mohammadmohsin@gmail.com>
2018-07-08 12:39:30 +0500

    Test minimum contribution & new request by manager.

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -52,4 +52,27 @@ describe('Campaigns', () => {
     const isContributor = await campaign.methods.approvers(accounts[1]).call()
     assert(isContributor)
   })
+
+  it('requires minimum contribution', async () => {
+    try{
+      await campaign.methods.contribute().send({
+        value: '5',
+        from: accounts[0]
+      })
+      assert(false)
+    } catch(err) {
+      assert(err)
+    }
+  })
+
+  it('allows a manger to make a payment request', async () => {
+    await campaign.methods
+                .createRequest('Buy batteries', '100', accounts[1])
+                .send({
+                  from: accounts[0],
+                  value: '1000000'
+                })
+    const request = await campaign.methods.requests(0).call()
+    assert.equal('Buy batteries', request.description)
+  })
 })
